4bbc796ddd9b9c893892665cd13779ecb29e4d91
Lee Stemkoski <stemkoski@gmail.com>
2022-01-31 09:48:29 -0500

    adding Vector class methods

--- GameProgramming/TestVector.java
+++ GameProgramming/TestVector.java
@@ -14,5 +14,34 @@ public class TestVector
         System.out.println("The vector is:");
         System.out.println( v );
         
+        System.out.println("The length is:");
+        System.out.println( v.getLength() );
+        
+        Vector w = new Vector(0, 5);
+        System.out.println( "The vector" );
+        System.out.println( w );
+        System.out.println( "has angle" );
+        System.out.println( w.getAngle() );
+        
+        w.setValues(0, 1);
+        System.out.println("The vector has changed to: " + w);
+        
+        System.out.println("Changing length to 2");
+        w.setLength(2);
+        System.out.println("The vector has changed to: " + w); // expect (0,2)
+
+        w.setValues(0, 1);
+        System.out.println("The vector has changed to: " + w);
+        System.out.println("Changing angle to 180 degrees");
+        w.setAngle(180);
+        System.out.println("The vector has changed to: " + w); // expect (-1, 0)
+
+        w.setValues(1, 2);
+        System.out.println("The vector has changed to: " + w);
+        System.out.println("adding values (5, 6)");
+        w.addValues(5, 6);
+        System.out.println("The vector has changed to: " + w); // expect (6, 8)
+
+    
     }
 }
--- GameProgramming/bagel/Vector.java
+++ GameProgramming/bagel/Vector.java
@@ -1,7 +1,7 @@
 package bagel;
 
 /**
- * Store a two-dimensional vector <x, y>.
+ * Store a two-dimensional vector (x, y).
  *
  * @author Stemkoski
  * @version 1.0
@@ -54,6 +54,73 @@ public class Vector
     {
         return Math.sqrt( x * x + y * y );
     }
+    
+    /**
+     * Calculate the direction of this vector: the angle between the vector
+     *   and the x-axis.
+     *
+     * @return the angle (direction) of this vector in degrees
+     */
+    public double getAngle()
+    {
+        double angleRadians = Math.atan2(y, x);
+        double angleDegrees = Math.toDegrees(angleRadians);
+        return angleDegrees;
+    }
+    
+    /**
+     * Change the values stored in the vector.
+     *
+     * @param a the new x value
+     * @param b the new y value
+     */
+    public void setValues( double a, double b )
+    {
+        x = a;
+        y = b;
+    }
+    
+    /**
+     * Change the length of this vector,
+     *   while keeping the current angle.
+     *
+     * @param newLength new length of this vector
+     */
+    public void setLength(double newLength)
+    {
+        double angleDegrees = getAngle();
+        double angleRadians = Math.toRadians(angleDegrees);
+        x = newLength * Math.cos(angleRadians);
+        y = newLength * Math.sin(angleRadians);
+    }
+    
+    /**
+     * Change the angle of this vector,
+     *   while keeping the current length.
+     *
+     * @param newAngleDegrees new angle of this vector, in degrees
+     */
+    public void setAngle(double newAngleDegrees)
+    {
+        double length = getLength();
+        double newAngleRadians = Math.toRadians(newAngleDegrees);
+        x = length * Math.cos(newAngleRadians);
+        y = length * Math.sin(newAngleRadians);
+    }
+    
+    /**
+     * Add the values (a, b) to the current position (x, y).
+     *
+     * @param a the value to add to x
+     * @param b the value to add to y
+     */
+    public void addValues(double a, double b)
+    {
+        x += a;
+        y += b;
+    }
+    
+    
     
     
 }
