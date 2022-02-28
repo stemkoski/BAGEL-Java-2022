49289dfd45b891a4338cb436085d8cd1215da168
Lee Stemkoski <stemkoski@gmail.com>
2022-02-28 09:50:27 -0500

    added Label class

---
+++ GameProgramming/TestLabel.java
@@ -0,0 +1,110 @@
+import bagel.*;
+
+/**
+ * Write a game that tests the new group class and methods.
+ */
+public class TestLabel extends Game
+{
+    Sprite turtle;
+    Sprite win;
+
+    Label scoreLabel;
+    
+    // two groups to store sprites:
+    //  "main": background image, turtle, etc.
+    //  "starfish": put all starfish here
+    public void initialize()
+    {
+        createGroup("main");
+        createGroup("starfish");
+
+        Sprite water = new Sprite();
+        water.setTexture( new Texture("water.png") );
+        water.setPosition( 0,0 );
+        water.setSize( 800,600 );
+        addSpriteToGroup( water, "main" );
+
+        turtle = new Sprite();
+        turtle.setTexture( new Texture("turtle.png") );
+        turtle.setPosition( 400,300 );
+        turtle.setSize(64, 64);
+        addSpriteToGroup( turtle, "main" );
+
+        // more efficient to load each texture once
+        Texture starfishTexture = new Texture("starfish.png");
+        int starfishCount = 100;
+        for (int i = 0; i < starfishCount; i++)
+        {
+            Sprite starfish = new Sprite();
+            starfish.setTexture( starfishTexture );
+            starfish.setSize(32, 32);
+            double x = (Math.random() * 700) + 50;
+            double y = (Math.random() * 500) + 50;
+            starfish.setPosition(x,y);
+            addSpriteToGroup( starfish, "starfish" );
+        }
+
+        win = new Sprite();
+        win.setTexture( new Texture("youwin.png") );
+        win.setPosition( 200, 200 );
+        win.setVisible( false );
+        addSpriteToGroup( win, "main" );
+        
+        scoreLabel = new Label();
+        scoreLabel.setText("Score: 0");
+        scoreLabel.setPosition( 50, 50 );
+        scoreLabel.setFont("Arial Bold", 48 );
+        // draw text in yellow
+        scoreLabel.setColor(1.00, 1.00, 0.00);
+        addSpriteToGroup( scoreLabel, "main" );
+        
+    }
+
+    public void update()
+    {
+        double speed = 5;
+        if (input.isKeyPressing("W"))
+            turtle.moveBy(0, -speed);
+        if (input.isKeyPressing("A"))
+            turtle.moveBy(-speed, 0);
+        if (input.isKeyPressing("S"))
+            turtle.moveBy(0, speed);
+        if (input.isKeyPressing("D"))
+            turtle.moveBy(speed, 0);
+
+            
+        for (Sprite starfish : getGroupSpriteList("starfish"))
+        {
+            if ( turtle.overlap(starfish) )
+            {
+                removeSpriteFromGroup(starfish, "starfish");
+            }
+        }
+
+        // we win the game when there are no starfish left
+        if ( getGroupSpriteCount("starfish") == 0 )
+        {
+            win.setVisible( true );
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            launch(args);
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            System.exit(0);
+        }
+    }
+}
+
+
+
+
---
+++ GameProgramming/bagel/Label.java
@@ -0,0 +1,101 @@
+package bagel;
+
+import javafx.scene.canvas.*;
+import javafx.scene.text.*;
+import javafx.scene.paint.Color;
+
+/**
+ * Display text in a game.
+ * Extending the Sprite class so Labels can be added to Groups,
+ *   and automatically drawn by the Game class.
+ */
+public class Label extends Sprite
+{
+    public String text;
+    
+    public Font font;
+    
+    public Color fontColor;
+    public Color borderColor;
+    
+    public Label()
+    {
+        text = "";
+        font = new Font("Arial Bold", 36);
+        fontColor = Color.BLACK;
+        borderColor = Color.BLACK;
+    }
+    
+    /**
+     * Change the text which is displayed by this label.
+     *
+     * @param t the text to be displayed.
+     */
+    public void setText(String t)
+    {
+        text = t;
+    }
+    
+    /**
+     * Change the font used to draw the text
+     *
+     * @param fontName name of the font to use 
+     *         (ex: "Arial", "Times New Roman", "Courier New", ....)
+     * @param fontSize base size of font (approximately height in pixels)
+     */
+    public void setFont(String fontName, int fontSize)
+    {
+        font = new Font(fontName, fontSize);
+    }
+    
+    /**
+     * Set the color of the text being drawn. Colors use RGB (red, green, blue) percents;
+     *   mix together as colors of light (not colors of paint).
+     *
+     * @param redPercent the amount of red light used
+     * @param greenPercent the amount of green light used
+     * @param bluePercent the amount of blue light used
+     */
+    public void setColor(double redPercent, double greenPercent, double bluePercent)
+    {
+        fontColor = Color.color(redPercent, greenPercent, bluePercent);
+    }
+    
+    /**
+     * Set the color of the text border being drawn. Colors use RGB (red, green, blue) percents;
+     *   mix together as colors of light (not colors of paint).
+     *
+     * @param redPercent the amount of red light used
+     * @param greenPercent the amount of green light used
+     * @param bluePercent the amount of blue light used
+     */
+    public void setBorderColor(double redPercent, double greenPercent, double bluePercent)
+    {
+        borderColor = Color.color(redPercent, greenPercent, bluePercent);
+    }
+    
+    /**
+     * Draw the text on the screen.
+     *
+     * @param context the context attached to the canvas where images are drawn
+     */
+    public void draw(GraphicsContext context)
+    {
+        // tell context which font to use when drawing text
+        context.setFont( font );
+        
+        // tell context which color to use when drawing text
+        context.setFill( fontColor );
+        
+        context.fillText(text, position.x, position.y);
+        
+        // tell context color to use around edges
+        context.setStroke( borderColor );
+        
+        context.strokeText(text, position.x, position.y);
+    }
+    
+    
+    
+    
+}
