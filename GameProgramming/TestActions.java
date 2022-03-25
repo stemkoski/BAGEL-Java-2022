b77631a5671027f947f109b8f278dfa2c29c36d8
Lee Stemkoski <stemkoski@gmail.com>
2022-03-25 09:54:38 -0400

    working on sequence and repeat Actions
    (code currently has an error)

--- GameProgramming/TestActions.java
+++ GameProgramming/TestActions.java
@@ -20,9 +20,47 @@ public class TestActions extends Game
         starfish.setSize(48, 48);
         starfish.setPosition(400, 300);
         
-        starfish.addAction( Action.rotateBy(360, 4) );
+        /*
+         
+        // by default: all Actions added to a Sprite are applied at the same time
+        starfish.addAction( Action.rotateBy(3600, 10) );
+        starfish.addAction( Action.moveBy(200, 100, 5) );
+
+        */
+       
+        /*
+        // test sequence of actions:
+        Action[] actionArray = { Action.moveBy(100,0, 2),  Action.moveBy(0,100, 2),
+                                 Action.moveBy(-100,0, 2), Action.moveBy(0,-100, 2) };
+                                 
+        starfish.addAction( Action.sequence( actionArray ) );
+        */
+       
+        // test repeat action
+        // starfish.addAction( Action.repeat( Action.rotateBy(360,1), 10000 ) );
+       
         
+        // test repeat a sequence of actions
+        Action[] actionArray = { Action.moveBy(100,0, 1),  Action.rotateBy(360, 1),
+                                 Action.moveBy(-100,0, 1), Action.rotateBy(360, 1) };
+                                 
+        starfish.addAction( Action.repeat( Action.sequence(actionArray), 10000 ) );
+       
         addSpriteToGroup(starfish, "main");
+        
+        
+        // add the turtle
+        Sprite turtle = new Sprite();
+        turtle.setTexture( new Texture("turtle.png") );
+        turtle.setSize(64,64);
+        turtle.setPosition(400, 100);
+        addSpriteToGroup( turtle, "main" );
+        
+        Action[] turtleActions = { Action.moveBy(200,0, 3),
+                                   Action.rotateBy(180, 1),
+                                   Action.moveBy(-200,0, 3) };
+
+        turtle.addAction( Action.sequence(turtleActions) );
         
     }
     
--- GameProgramming/bagel/Action.java
+++ GameProgramming/bagel/Action.java
@@ -1,6 +1,5 @@
 package bagel;
 
-
 /**
  * This class contains a function that is applied to a Sprite
  *   automatically over time.
@@ -12,19 +11,19 @@ public class Action
     //  * Sprite -- target of the action
     //  * delta Time -- the amount of time that has passed since last update (1/60)
     //  * total Time -- the amount of time passed since the Action started
-    
+
     //  * return a boolean: true if the Action is finished.
-    
+
     public interface Function
     {
         public boolean run(Sprite target, double deltaTime, double totalTime);
     }
-    
+
     // the actual function instance applied to Sprite
     public Function function;
     // how long this Action has been running
     public double totalTime;
-    
+
     /**
      * Creates an Action object, which contains a function applied to a Sprite
      *   automatically over time.
@@ -35,7 +34,7 @@ public class Action
     {
         function = f;
     }
-    
+
     /**
      * Apply the Action's function to a Sprite.
      *
@@ -50,11 +49,20 @@ public class Action
         // run the function on a sprite
         return function.run(target, deltaTime, totalTime);
     }
+
+    /**
+     * To re-use an action, need to reset the total running time to 0.
+     * (Resetting a sequence is slightly different... need to reset totalTime of multiple actions.)
+     */
+    public void reset()
+    {
+        totalTime = 0;
+    }
+
     
     // methods to create Action objects
     // static methods, to more easily create Actions directly from class
-    
-    
+
     /**
      * Create an Action containing a function that automatically moves a Sprite
      *   over some amount of time.
@@ -67,20 +75,20 @@ public class Action
     public static Action moveBy(double deltaX, double deltaY, double duration)
     {
         Function func = new Function()
-        {
-            public boolean run(Sprite target, double deltaTime, double totalTime)
             {
-                target.moveBy( deltaX * deltaTime/duration,
-                               deltaY * deltaTime/duration  );
-                // this action is finished, once it has been running (totalTime)
-                //   for the desired duration of time.
-                boolean finished = (totalTime >= duration);
-                return finished;
-            }
-        };
+                public boolean run(Sprite target, double deltaTime, double totalTime)
+                {
+                    target.moveBy( deltaX * deltaTime/duration,
+                        deltaY * deltaTime/duration  );
+                    // this action is finished, once it has been running (totalTime)
+                    //   for the desired duration of time.
+                    boolean finished = (totalTime >= duration);
+                    return finished;
+                }
+            };
         return new Action(func);   
     }
-    
+
     /**
      * Create an Action containing a function that automatically rotates a Sprite
      *   over some amount of time.
@@ -92,17 +100,94 @@ public class Action
     public static Action rotateBy(double deltaA, double duration)
     {
         Function func = new Function()
-        {
-            public boolean run(Sprite target, double deltaTime, double totalTime)
             {
-                target.rotateBy( deltaA * deltaTime/duration );
-                boolean finished = (totalTime >= duration);
-                return finished;
-            }
-        };
+                public boolean run(Sprite target, double deltaTime, double totalTime)
+                {
+                    target.rotateBy( deltaA * deltaTime/duration );
+                    boolean finished = (totalTime >= duration);
+                    return finished;
+                }
+            };
         return new Action(func);
     }
-    
+
+    /**
+     * Perform a sequence of Actions, one after the other, instead of running all
+     *   Actions at the same time (which is the default).
+     *
+     * @param actionArray an array of Actions
+     * @return Action
+     */
+    public static Action sequence(Action[] actionArray)
+    {
+        return new Action(null)
+        {
+            public int actionIndex = 0;
+
+            public Action()
+            {
+                function = new Function() 
+                {
+                    public boolean run(Sprite target, double deltaTime, double totalTime)
+                    {
+                        Action currentAction = actionArray[ actionIndex ];
+                        boolean finished = currentAction.apply( target, deltaTime );
+                        // if the current action is finished, next time, move on to next action
+                        if (finished == true)
+                            actionIndex++;
+                        // if all actions are finished, the sequence is finished
+                        boolean sequenceFinished = (actionIndex == actionArray.length);
+
+                        return sequenceFinished;
+                    }
+                };
+            }
+            // override the reset function
+            public void reset()
+            {
+                // reset all Actions contained in actionArray.
+                for (int i = 0; i < actionArray.length; i++)
+                {
+                    Action act = actionArray[i];
+                    act.reset();
+
+                    // start sequence at index 0 again
+                    actionIndex = 0;
+                }
+            }
+        };
+    }
+
+    /**
+     * Repeats an action by a given number of times
+     *  by resetting it every time it is finished.
+     *
+     * @param act the action to repeat
+     * @param numberOfTimes how many times the action should be repeated
+     * @return Action
+     */
+    public static Action repeat(Action act, int numberOfTimes)
+    {
+        Function func = new Function()
+            {
+                public int finishedCount = 0;
+
+                public boolean run(Sprite target, double deltaTime, double totalTime)
+                {
+                    boolean finished = act.apply(target, deltaTime);
+                    if (finished == true)
+                    {
+                        act.reset();
+                        finishedCount++;
+                    }
+
+                    return (finishedCount == numberOfTimes);
+                }
+            };
+
+        return new Action(func);
+    }
+
     
     
 }
