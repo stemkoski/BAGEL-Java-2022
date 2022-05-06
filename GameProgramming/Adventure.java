68aca175a39aa00dffa583008e397ec8aebaf346
Lee Stemkoski <stemkoski@gmail.com>
2022-05-06 09:53:58 -0400

    adventure game additions

--- GameProgramming/Adventure.java
+++ GameProgramming/Adventure.java
@@ -25,7 +25,7 @@ public class Adventure extends Game
 
     // store current player direction angle
     //  for use in shooting arrows.
-    
+
     public int playerDirection;
 
     public void initialize()
@@ -37,9 +37,9 @@ public class Adventure extends Game
         // f = flyingRandom enemy location
         // g = groundTracker enemy location
         // R = rocks (solid; provide protection from tracking enemies)
-        
+
         String[] mapDataRows = {"ABBBBBBBBBBBBBBBBBBC",
-                "D.:............g...F",
+                "D.:..f.........g...F",
                 "D....;.............F",
                 "Dg.ABBBBBC..f...;..F",
                 "D..D.....F.........F",
@@ -48,23 +48,23 @@ public class Adventure extends Game
                 "D..D.....J....:....F",
                 "D:.D...,.F.........F",
                 "D..GHK.LHI.........F",
-                "D......:....f..R;..F",
+                "D......:..g.f..R;..F",
                 "D.,........,.....,.F",
                 "D..f....R........f.F",
                 "D..............,...F",
                 "D.R...,.....f......F",
                 "D..........,R......F",
                 "D..:g...........g..F",
-                "D..............;...F",
+                "D........f.....;...F",
                 "D..................F",
                 "GHHHHHHHHHHHHHHHHHHI" };
 
         String[] mapSymbolArray = { ".", ",", ":", ";", 
-                                    "A", "B", "C", "D", "E", "F", "G", "H", "I", "P", 
-                                      "J", "K", "L", "M", "f", "g", "R" };
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "P", 
+                "J", "K", "L", "M", "f", "g", "R" };
         int[]    regionIndexArray = { 19, 25, 26, 32, 
-                                      5, 6, 7, 13, 14, 15, 21, 22, 23, 19, 
-                                      36, 37, 38, 39, 19, 19, 19 };
+                5, 6, 7, 13, 14, 15, 21, 22, 23, 19, 
+                36, 37, 38, 39, 19, 19, 19 };
 
         map.loadMapData( mapDataRows, mapSymbolArray, regionIndexArray );
         map.setTileSize( 48, 48 );
@@ -88,7 +88,17 @@ public class Adventure extends Game
         addSpriteToGroup( player, "main" );
 
         playerDirection = 90;
-        
+
+        createGroup("rock");
+        for (Vector position : map.getSymbolPositionList("R"))
+        {
+            Sprite rock = new Sprite();
+            rock.setTexture( new Texture("images/rock.png") );
+            rock.setSize(48, 48);
+            rock.setPosition(position.x, position.y);
+            addSpriteToGroup(rock, "rock");
+        }
+
         // create enemy groups; different groups for different behavior
         createGroup("enemy-ground");
         createGroup("enemy-flying");
@@ -100,11 +110,24 @@ public class Adventure extends Game
             enemy.setAnimation( enemyAnim );
             enemy.setSize(50, 40);
             enemy.setPosition( position.x, position.y );
-            enemy.setPhysics( new Physics(0, 100, 0) );
+            enemy.setPhysics( new Physics(0, 200, 0) );
             enemy.physics.setSpeed(100);
             addSpriteToGroup( enemy, "enemy-flying" );
         }
         
+        for (Vector position : map.getSymbolPositionList("g"))
+        {
+            Sprite enemy = new Sprite();
+            Animation enemyAnim = new Animation("images/enemy-seeker.png", 1, 4, 0.1, true);
+            enemy.setAnimation( enemyAnim );
+            enemy.setSize(40, 40);
+            enemy.setPosition(position.x, position.y);
+            enemy.setPhysics( new Physics(0, 90, 0) );
+            enemy.physics.setSpeed(90);
+            addSpriteToGroup( enemy, "enemy-ground" );
+        }
+        
+
         // health stuff
         health = 5;
         healthLabel = new Label();
@@ -112,7 +135,7 @@ public class Adventure extends Game
         healthLabel.setText( "Health: " + health );
         healthLabel.setPosition( 20, 50 );
         addSpriteToGroup( healthLabel, "main" );
-        
+
         createGroup("arrow");
         /*
         // enemies
@@ -142,9 +165,13 @@ public class Adventure extends Game
 
         Action[] actionArray = { Action.delay(1), Action.rotateBy(45, 1) };
         flyingRandom.addAction( Action.sequence( actionArray ) );
-        */
+         */
+
+        // used for item (gain 1 health)
+        createGroup("heart");
 
     }
+
     public void update()
     {
         if ( input.isKeyPressing("W") )
@@ -176,53 +203,45 @@ public class Adventure extends Game
 
         // prevent overlap with solid map tiles and player
         map.preventOverlap( player );
-        
-        /*
-        for (Sprite enemy : getGroupSpriteList("enemy") )
-        {
-            // also check that player is *not* currently flashing;
-            //  in other words: player should have *no* actions currently in process.
-            int playerActionCount = player.actionList.size();
 
-            if ( player.overlap(enemy) && playerActionCount == 0 )
+        // prevent player from overlapping rocks
+        for (Sprite rock : getGroupSpriteList("rock"))
+        {
+            if (player.overlap(rock))
             {
-                health -= 1;
-                healthLabel.setText("Health: " + health);
-                player.addAction( Action.flashRepeat(10) );
+                player.preventOverlap(rock);
             }
         }
-        */
 
-        /*
-        // enemy movement code
-        Vector vectorToPlayer = new Vector();
-        vectorToPlayer.x = player.position.x - flyingTracker.position.x;
-        vectorToPlayer.y = player.position.y - flyingTracker.position.y;
-        double angleToPlayer = vectorToPlayer.getAngle();
-        flyingTracker.physics.setMotionAngle(angleToPlayer);
-
-        // move ground enemy
-        vectorToPlayer.x = player.position.x - groundTracker.position.x;
-        vectorToPlayer.y = player.position.y - groundTracker.position.y;
-        angleToPlayer = vectorToPlayer.getAngle();
-        groundTracker.physics.setMotionAngle(angleToPlayer);
-        // make turtle face player
-        groundTracker.angle = angleToPlayer;
-        // ground enemy can not overlap solid map tiles
-        map.preventOverlap(groundTracker);
-
-        // move random flying enemy
-        
-        flyingRandom.physics.setMotionAngle( flyingRandom.angle );
-        if (flyingRandom.actionList.size() == 0)   
+        for (Sprite enemy : getGroupSpriteList("enemy-ground"))
         {
-            double angle = 360 * Math.random() - 180;
-            Action[] actionArray = { Action.delay(1), Action.rotateBy(angle, 0.5) };
-            flyingRandom.addAction( Action.sequence( actionArray ) );
+            Vector direction = new Vector();
+            direction.x = player.position.x - enemy.position.x;
+            direction.y = player.position.y - enemy.position.y;
+            double angle = direction.getAngle();
+            // enemy faces player
+            enemy.angle = angle;
+            // enemy moves towards player
+            enemy.physics.setMotionAngle(angle);
+            
+            // block movement through solids
+            map.preventOverlap(enemy);
+            // also check rocks
+            for (Sprite rock : getGroupSpriteList("rock"))
+            {
+                enemy.preventOverlap(rock);
+            }
+            // prevent enemies from overlapping with each other
+            for (Sprite enemy2 : getGroupSpriteList("enemy-ground"))
+            {
+                // check that enemies are different!
+                if ( !enemy.equals(enemy2) )
+                {
+                    enemy.preventOverlap(enemy2);
+                }
+            }
         }
-        flyingRandom.wrap(975, 985);
-        */
-       
+        
         for (Sprite enemy : getGroupSpriteList("enemy-flying"))
         {
             if (enemy.actionList.size() == 0)
@@ -231,12 +250,16 @@ public class Adventure extends Game
                 double angle = 360 * Math.random() - 180;
                 Action[] actionArray = { Action.delay(delayTime), Action.rotateBy(angle, 0.5) };
                 enemy.addAction( Action.sequence( actionArray ) );
+
+                // also change enemy speed
+                double speed = 100 + (100 * Math.random());
+                enemy.physics.setSpeed(speed);
             }
-            
+
             // things that need to happen during every frame
             enemy.physics.setMotionAngle(enemy.angle);
             enemy.wrap( 975, 985 );
-            
+
             // check for player damage
             int playerActionCount = player.actionList.size();
             if (player.overlap(enemy) && playerActionCount == 0)
@@ -246,15 +269,17 @@ public class Adventure extends Game
                 player.addAction( Action.flashRepeat(10) );
             }
         }
-       
-        if (input.isKeyPressed("SPACE"))
+
+        // limit arrows to N at a time.
+        int arrowCount = getGroupSpriteCount("arrow");
+        if (input.isKeyPressed("SPACE") && arrowCount < 1)
         {
             Sprite arrow = new Sprite();
             arrow.setTexture( new Texture("images/arrow.png") );
-            arrow.setSize( 30, 15 );
+            arrow.setSize( 40, 20 );
             arrow.alignToSprite(player);
-            arrow.setPhysics( new Physics(0, 300, 0) );
-            arrow.physics.setSpeed(300);
+            arrow.setPhysics( new Physics(0, 500, 0) );
+            arrow.physics.setSpeed(500);
             arrow.physics.setMotionAngle(playerDirection);
             arrow.angle = playerDirection;
             // arrows disappear after 2 seconds
@@ -262,7 +287,80 @@ public class Adventure extends Game
             arrow.addAction( Action.sequence(actionArray) );
             addSpriteToGroup(arrow, "arrow");
         }
-       
+
+        // destroy enemies:
+        for ( Sprite arrow : getGroupSpriteList("arrow") )
+        {
+            // check for overlap with each type of enemy
+            for ( Sprite enemy : getGroupSpriteList("enemy-flying") )
+            {
+                if (arrow.overlap(enemy))
+                {
+                    enemy.destroy();
+                    // do not destroy arrow, so it flies through and can hit multiple enemies
+
+                    // explosion
+                    Sprite explosion = new Sprite();
+                    Animation explosionAnim = 
+                        new Animation("images/explosion.png", 6, 6, 0.01, false);
+                    explosion.setAnimation(explosionAnim);
+                    explosion.setSize(100,100);
+                    explosion.alignToSprite(enemy);
+                    addSpriteToGroup(explosion, "main");
+
+                    // random item (health) drop
+                    if (Math.random() < 0.25)
+                    {
+                        Sprite heart = new Sprite();
+                        heart.setTexture( new Texture("images/heart.png") );
+                        heart.setSize(50,50);
+                        heart.alignToSprite(enemy);
+                        heart.angle = 0;
+                        addSpriteToGroup(heart, "heart");
+                    }
+                }
+            }
+            
+            for ( Sprite enemy : getGroupSpriteList("enemy-ground") )
+            {
+                if (arrow.overlap(enemy))
+                {
+                    enemy.destroy();
+                    // do not destroy arrow, so it flies through and can hit multiple enemies
+
+                    // explosion
+                    Sprite explosion = new Sprite();
+                    Animation explosionAnim = 
+                        new Animation("images/explosion.png", 6, 6, 0.01, false);
+                    explosion.setAnimation(explosionAnim);
+                    explosion.setSize(100,100);
+                    explosion.alignToSprite(enemy);
+                    addSpriteToGroup(explosion, "main");
+
+                    // random item (health) drop
+                    if (Math.random() < 0.25)
+                    {
+                        Sprite heart = new Sprite();
+                        heart.setTexture( new Texture("images/heart.png") );
+                        heart.setSize(50,50);
+                        heart.alignToSprite(enemy);
+                        heart.angle = 0;
+                        addSpriteToGroup(heart, "heart");
+                    }
+                }
+            }
+        }
+
+        for (Sprite heart : getGroupSpriteList("heart"))
+        {
+            if (player.overlap(heart))
+            {
+                heart.destroy();
+                health += 1;
+                healthLabel.setText("Health: " + health);
+            }
+        }
+
        
     }
 
